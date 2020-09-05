247251ae7e7be087885f2f3556f6f470033b1fc4
Timothy Demarest <demarest@gmail.com>
2020-09-04 18:26:57 -0700

    Major change to latest Slack API and official Python Slack client

--- README.md
+++ README.md
@@ -1,17 +1,97 @@
 # slack-bulkinviter
 
-Super quick and dirty Python script to bulk invite ALL users in a slack team to a specific channel. We need to do this regulary with [Counterparty](http://www.counterparty.io) when we create a new channel that everyone should be in, or move things around. Now using the new Conversations API.
+Python script to bulk invite **all** users in a slack team to a specific channel.
+While there are a number of ways to do this with small teams (see below), this
+script uses the official Slack Python client and latest features for pagination
+ensuring that one user to tens of thousands of users can easily be bulk
+invited to a channel.
 
-To use:
-* Install the [slacker](https://github.com/os/slacker) library via `pip`
-* [Get an API key](https://get.slack.help/hc/en-us/articles/215770388-Creating-and-regenerating-API-tokens)
-* Optionally, if not using `--apikey`, create a file in the same directory as `slack-bulkinviter.py` containing your API key and pass the filename to the argument `--apikey` or `-k`
-* Execute the script, passing the name of the channel where all users will be invited, such as `./slack-bulkinviter.py --channel mychannel --apikey myapistring`
-* Sit back and let it do its work
+Updated to use the new Conversations API and the [official Python slackclient](
+https://github.com/slackapi/python-slackclient) now that
+[slacker](https://github.com/os/slacker) has been archived and no longer supported.
+
+Basic usage:
+
+* Python 3.x is required.
+* Install the [python-slackclient](https://github.com/slackapi/python-slackclient)
+library via `pip`
+* :warning: Legacy Slack API tokens have been deprecated. Create a [Slack App](https://api.slack.com/apps)
+for your workspace.
+* Add an App OAuth token for the app you just create. More details on this can be
+found at [Create and regenerate API tokens](https://get.slack.help/hc/en-us/articles/215770388-Creating-and-regenerating-API-tokens).
+* Set the OAuth Scope using just the `User Token Scope`. These are the minimum
+scope permisisons required for this script:
+
+Permission | Description
+-----------| -----------
+channels:read | View basic information about public channels in the workspace
+channels:write | Manage the user’s public channels and create new ones on the user’s behalf
+groups:read | View basic information about the user’s private channels
+users:read | View people in the workspace
+
+* Install or reinstall the app once the OAuth scope has been defined or updated.
+* :no_entry: Using a file for the token has been deprecated. Use the `--token`
+option or set or pass the OAuth token as an environment variable to the script.
+* Execute the script, passing the name of the channel where all users will be
+invited, such as:
+
+```shell
+./slack-bulkinviter.py --token MyOAuthToken --channel mychannel
+```
+
+or by passing the OAuth token as an environemnt variable:
+
+```shell
+env SLACK_API_TOKEN=my-slack-app-oauth-token ./slack-bulkinviter.py --channel mychannel
+```
+
+* Sit back and let it do its work. Large workspaces can take a long time to process.
+Using the `--verbose` option is recommended in workspaces with more than 1000
+users or channels.
+
+## Script Options
+
+Option | Descriptions
+------ | ------------
+`--help` | Help on using the script.
+`--channel` | The channel to add the user to. Required.
+`--token` | The Slack OAuth Token (string) for the user app required for the workspace. Required if the `$SLACK_API_TOKEN` environment variable is not set.
+`--bots` | Include bot users in the channel. Excluded by default.
+`--apps` | Include apps in the channel. Excluded by default.
+`--sleep` | Number of seconds to sleep between pagination calls. Default is as low as is reasaonable. default: 2 seconds.
+`--split` | List split divisor used to chunk large lists into smaller chunks. This is required if the invite list exceeds 1000 users. If the invite list is greater than 10,0000 use `--split` to increase the divisor until `Users = Total Invited Users / Split` is less than 1000. Defaults to **10** which is good for up to 10,000 invites.
+`--verbose` | Output more info about what is happening. Useful for workspaces with large number of conversations or users.
+
+## Alternatives to this script
+
+### UI: Channel Add Members
+
+As of today using the UI option for a channel "add people to" `+` option allows
+pasting over 2000 email addresses. This option either didn't exist or wouldn't
+allow more than 100 people in the past. it may be likely that there is no limit
+on this but it's been tested by me for just under 3000 email addresses.
+
+### `/who` Cut and Paste
+
+The `/who` command in a channel will return the first 99 users. For small
+organizations this may be sufficient. Type `/who` in the most populated channel,
+usually `#general` then copy the list of users and use as the input to `/invite`
+in the channel you want to invite everyone.
+
+### Channel Tools
+
+Add [Channel Tools](https://www.channeltools.io/) to your workspace. Depends on the users
+from one channel that is used as the basis for the invites for the target channel.
+
+### Other
+
+See [StackExchange](https://webapps.stackexchange.com/questions/100820/how-do-i-invite-all-team-members-to-a-new-slack-channel/123420#123420)
+has some solutions as well. Make sure to read all of the comments.
 
 ## Run from Docker
 
-Docker instructions courtesy of [@markshust](https://github.com/markshusthttps://github.com/markshust).
+Docker instructions courtesy of [@markshust](https://github.com/markshust)
+(modified slightly to use the env variable for the OAuth token).
 
 Create a new directory named "slack-bulkinviter", then once in that directory, copied the raw file there:
 
@@ -19,8 +99,6 @@ Create a new directory named "slack-bulkinviter", then once in that directory, c
 mkdir slack-bulkinviter && cd $_
 curl -O https://raw.githubusercontent.com/robby-dermody/slack-bulkinviter/master/slack-bulkinviter.py
 ```
-
-Place your `apikey.txt` file in this same directory with the contents of your API key.
 
 Create a file named `Dockerfile` with the following contents:
 
@@ -30,7 +108,6 @@ FROM python:3.8.2-buster
 RUN pip install --no-cache-dir slacker
 
 COPY slack-bulkinviter.py .
-COPY apikey.txt .
 ```
 
 Build the docker image with:
@@ -39,5 +116,7 @@ Build the docker image with:
 
 At this point you should be all set, and can run the script like so:
 
-`docker run -it --rm slack-bulkinviter python slack-bulkinviter.py your-channel`
-
+```shell
+docker run --env SLACK_API_TOKEN=my-own-slack-oath-token -it --rm \
+slack-bulkinviter python slack-bulkinviter.py --channel your-channel
+```
--- slack-bulkinviter.py
+++ slack-bulkinviter.py
@@ -1,76 +1,188 @@
 #! /usr/bin/env python3
+
 import sys
-import argparse
+import logging
+import os
 import time
-from slacker import Slacker, Error
+import argparse
+from slack import WebClient
+
 
 # Initiate the parser
 parser = argparse.ArgumentParser()
 
 # add long and short argument
-parser.add_argument("-c", "--channel", required=True, metavar="<Channel Name>", help="Set channel name to add members")
-parser.add_argument("-k", "--apikey", metavar="'API Key Text String'", help="API Key string")
-parser.add_argument("-f", "--apifile", metavar="<filename>", help="Path to API Key file - only required is not using --apikey|-k")
-parser.add_argument("-b", "--bots", action='store_true', help="Include bots in channel")
-parser.add_argument("-a", "--apps", action='store_true', help="Include apps in channel")
+parser.add_argument("-c", "--channel", required=True, metavar="<Channel Name>", help="Channel name to add members. REQUIRED.")
+parser.add_argument("-k", "--token", metavar="OAuth Access Token Text String'", help="Slack App OAuth access token string")
+parser.add_argument("-b", "--bots", action='store_true', help="Include bots in channel. default: not included.")
+parser.add_argument("-a", "--apps", action='store_true', help="Include apps in channel. default: not included.")
+parser.add_argument("--sleep", type=int, default=2, help="Adjust sleep time between pagination calls. default: 2 seconds")
+parser.add_argument("-s", "--split", type=int, default=10, help="Split invite list into less than 1000 users. If your invite list is greater than 10000 users adjust this to split into less than 1000 chunks (as a divisor). default: 10")
+parser.add_argument("-v", "--verbose", action='store_true', help="Verbose output. Useful inworkspaces with large number of users or conversations.")
 
 # read arguments from the command line
 args = parser.parse_args()
-if args.apikey is None and args.apifile is None:
-    parser.error("You must pass an API Text String using --apikey or path to an API Key File using --apifile")
 
-if args.apikey:
-    slack = Slacker(args.apikey)
+if args.verbose:
+    # Setup logging using f-string formatting
+    # lmgging.basicConfig(stream=sys.stderr, level=logging.INFO, format="{processName:<12} {message} ({filename}:{lineno})", style="{")
+    logging.basicConfig(stream=sys.stderr, level=logging.INFO, format="{message} ({filename}:{lineno})", style="{")
+
+if args.token:
+    client = WebClient(args.token)
 else:
-    # Load API key from apikey.txt
+    # Load API key from environment variable
     try:
-        with open(args.apifile) as f:
-            api_key = f.read().strip()
-            assert api_key
-    except IOError:
-        print("Cannot find API Key file {}, or other reading error".format(args.apifile))
-        sys.exit(1)
+        token = os.environ["SLACK_API_TOKEN"]
+        assert token
     except AssertionError:
-        print("Empty API key")
-        sys.exit(1)
+        print('No OAuth Token passed via --token or env SLACK_API_TOKEN')
     else:
-        slack = Slacker(api_key)
+        client = WebClient(token)
 
-# Get channel id from name
-response = slack.conversations.list()
-channels = [d for d in response.body['channels'] if d['name'] == args.channel]
-if not len(channels):
-    print("Cannot find channel")
-    sys.exit(1)
-assert len(channels) == 1
-channel_id = channels[0]['id']
 
-# Get users list
-response = slack.users.list()
+def find_channel(response):
+    """
+    Iterate over the response channels list to determine if we
+    matched our channel
+    """
+
+    channels = None
+    channels = [d for d in response["channels"] if d['name'] == args.channel]
+    # for d in response["channels"]:
+    #   if d['name'] == args.channel:
+    #       channels = [d]
+
+    # if not len(channels):
+    if not channels:
+        logging.info(f'\U0001F5DE  Did not find channel {args.channel} on page {response["response_metadata"]["next_cursor"]}')
+        return()
+
+    assert len(channels) == 1
+
+    print(f'\N{bottle with popping cork} Found {len(channels)} channel matching {args.channel} with name of {channels[0]["name"]} and channel ID {channels[0]["id"]}')
+    channel_id = channels[0]['id']
+
+    return channel_id
+
 
 def should_invite(user):
+    """
+    Detemine if the current user needs to be invited
+    """
     return ((not user['deleted'])
-        and (not user['is_restricted'])
-        and (not user['is_ultra_restricted'])
-        and (args.bots or not user['is_bot'])
-        and (args.apps or not user['is_app_user'])
-        and (user['id'] != "USLACKBOT")
-    )
-users_all = set(u['id'] for u in response.body['members'] if should_invite(u))
+            and (not user['is_restricted'])
+            and (not user['is_ultra_restricted'])
+            and (args.bots or not user['is_bot'])
+            and (args.apps or not user['is_app_user'])
+            and (user['id'] != "USLACKBOT")
+            )
+
+
+def split_list(alist, wanted_parts=args.split):
+    """
+    Split a list into a number of "parts". This is used to make sure
+    to split into less than 1000 items due to API limits. Default is a
+    divisor of 10 which will split 10000 users into 10 lists of 1000.
+    Can be set from --split|-s at runtime.
+    """
+    length = len(alist)
+    return [alist[i * length // wanted_parts: (i + 1) * length // wanted_parts]
+            for i in range(wanted_parts)]
+
+
+if args.verbose:
+    logging.info(f'\U0001F30E  Verbose logging enabled {args.verbose}. Settings:')
+    logging.info(f'  API Token: {token}')
+    logging.info(f'  Channel: {args.channel}')
+    logging.info(f'  Sleep seconds: {args.sleep}')
+    logging.info(f'  Split: {args.split}')
+    logging.info(f'  Invite bots?: {args.bots}')
+    logging.info(f'  Invite apps?: {args.apps}')
+
+
+count = 0
+
+print(f'\N{stopwatch}  Starting search for channel named {args.channel}. This can take a while if the workspace has a lot of channels...')
+
+# Get channel id from name
+response = client.conversations_list(types="public_channel, private_channel", exclude_archived="true", limit=200)
+
+# Use pagination to make sure all channels are checked using limits
+while response["response_metadata"]['next_cursor']:
+    count += 1
+    next_page = response["response_metadata"]['next_cursor']
+    logging.info(f'\U0001F5DE  Conversation list cursor count {count} at index {next_page} and returned {len(response["channels"])} channels')
+    channel_id = find_channel(response)
+    if channel_id:
+        break
+    else:
+        time.sleep(args.sleep)
+        response = client.conversations_list(types="public_channel, private_channel", exclude_archived="true", cursor=next_page, limit=200)
+
+if not channel_id:
+
+    print(f'\U0001F6D1  Channel {args.channel} was not found.')
+    sys.exit(1)
+
+print('\N{stopwatch}  Grabbing the complete list of all users. This can take a while in large organizations...')
+
+# Get all of the users in the workspace which includes deleted and deactivated users
+# The API doesn't have a way to exclude deleted or deactivated users at this time
+count = 0
+response = client.users_list(limit=200)
+
+users_all = set(u['id'] for u in response['members'] if should_invite(u))
+
+while response["response_metadata"]['next_cursor']:
+    count += 1
+    next_page = response["response_metadata"]['next_cursor']
+    logging.info(f'\U0001F5DE  User list cursor is at count {count} and index {next_page} and returned {len(response["members"])} users')
+    users_all |= set(u['id'] for u in response['members'] if should_invite(u))
+    time.sleep(args.sleep)
+    response = client.users_list(cursor=next_page, limit=200)
+
+print(f'\N{chequered flag}  Number of total users (active, deleted & deactivated) is in workspace: {len(users_all)}')
+print(f'\N{stopwatch}  Grabbing members already in {args.channel}...')
 
 # Filter out people already in the channel
-response = slack.conversations.members(channel=channel_id, limit=1000)
-users_already_in = set(response.body['members'])
+count = 0
+response = client.conversations_members(channel=channel_id, limit=200)
+
+users_already_in = set(response["members"])
+
+while response["response_metadata"]['next_cursor']:
+    count += 1
+    next_page = response["response_metadata"]['next_cursor']
+    logging.info(f'\U0001F5DE  Channel member user list cursor is at count {count} and index {next_page} and returned {len(response["members"])}')
+    users_already_in |= set(response["members"])
+    time.sleep(args.sleep)
+    response = client.conversations_members(channel=channel_id, cursor=next_page, limit=200)
+
+print(f'\N{chequered flag}  Number of users already in channel {args.channel} is: {len(users_already_in)}')
 
 users_to_invite = users_all.difference(users_already_in)
 
+print(f'\N{chequered flag}  Number of users to invite to channel {args.channel} is: {len(users_to_invite)}')
+
 # Invite all users to slack channel
 if users_to_invite:
-    print("Inviting {} users to {}".format(len(users_to_invite), args.channel))
+    if len(users_to_invite) > 999:
+        logging.info('\U0001F5DE  Number of users is > 999 - chunking invites')
+        users_to_invite = split_list(list(users_to_invite), args.split)
+    else:
+        user_invite_chunk = users_to_invite
     try:
-        slack.conversations.invite(channel_id, users=list(users_to_invite))
-        print("Done")
+        count = 1
+        for user_invite_chunk in users_to_invite:
+            logging.info(f'\U0001F5DE  Length of invite chunk #{count}: {len(user_invite_chunk)}')
+            print(f'\U0001F39F  Inviting {len(user_invite_chunk)} users in chunk #{count} to channel {args.channel}...')
+            client.conversations_invite(channel=channel_id, users=user_invite_chunk)
+            time.sleep(args.sleep)
+            count += 1
     except Error as e:
         print("Error: " + e.args[0])
 else:
-    print("All users are already in the channel")
+    print("\N{chequered flag}  All users are already in the channel - nothing to do.")
+
+print("\N{chequered flag}  Done!")
